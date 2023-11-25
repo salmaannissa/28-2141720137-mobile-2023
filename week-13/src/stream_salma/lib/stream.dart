f2dbde8673a03a4c500d8b0be9fd211c2746270d
SALMA ANNISSA AZIZI <salmaannisaazizi@gmail.com>
2023-11-26 01:25:20 +0700

    W13: Soal 2

---
+++ week-13/src/stream_salma/lib/stream.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ColorStream {
+  final List<Color> colors = [
+    Colors.blueGrey,
+    Colors.amber,
+    Colors.deepPurple,
+    Colors.lightBlue,
+    Colors.teal,
+    // SOAL 2 : Menambahkan 5 warna lain
+    Colors.red.shade300,
+    Colors.indigo.shade300,
+    Colors.lightGreen.shade300,
+    Colors.lime.shade300,
+    Colors.deepOrange.shade300,
+  ];
+}
