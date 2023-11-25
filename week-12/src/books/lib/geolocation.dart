e89040841519ff3411feda8adeca96703fb24cbf
SALMA ANNISSA AZIZI <salmaannisaazizi@gmail.com>
2023-11-25 16:52:47 +0700

    W12: Soal 13

--- week-12/src/books/lib/geolocation.dart
+++ week-12/src/books/lib/geolocation.dart
@@ -10,35 +10,54 @@ class LocationScreen extends StatefulWidget {
 
 class _LocationScreenState extends State<LocationScreen> {
   String myPosition = '';
+  Future<Position>? position; // SOAL 13
   @override
   void initState() {
     super.initState();
-    getPosition().then((Position myPos) {
-      myPosition = 
-      'Latitude ${myPos.latitude.toString()} - Longitude: ${myPos.longitude.toString()}';
-      setState(() {
-        myPosition = myPosition;
-      });
-    });
+    position = getPosition(); // SOAL 13
+    // SOAL 12
+    // getPosition().then((Position myPos) {
+    //   myPosition = 
+    //   'Latitude ${myPos.latitude.toString()} - Longitude: ${myPos.longitude.toString()}';
+    //   setState(() {
+    //     myPosition = myPosition;
+    //   });
+    // });
   }
 
   @override
   Widget build(BuildContext context) {
     // SOAL 12
-    final myWidget = myPosition == ''
-    ? const CircularProgressIndicator()
-    : Text(myPosition);;
+    // final myWidget = myPosition == ''
+    // ? const CircularProgressIndicator()
+    // : Text(myPosition);
     return Scaffold(
       appBar: AppBar(title: const Text('Current Location || Salma Annissa')), // SOAL 11
       // body: Center(child: Text(myPosition)),
-      body: Center(child: myWidget), // SOAL 12
-    );
+      // body: Center(child: myWidget), // SOAL 12
+      // SOAL 13
+      body: Center(child: FutureBuilder(
+        future: position,
+        builder: (BuildContext context, AsyncSnapshot<Position> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const CircularProgressIndicator();
+          }
+          else if (snapshot.connectionState == ConnectionState.done) {
+            return Text(snapshot.data.toString());
+          }
+          else {
+            return const Text('');
+          }
+        },
+      ),
+    ));
   }
   Future<Position> getPosition() async {
-    await Future.delayed(const Duration(seconds: 3));
+    await Future.delayed(const Duration(seconds: 3)); // SOAL 12
     await Geolocator.requestPermission();
     await Geolocator.isLocationServiceEnabled();
-    Position? position = await Geolocator.getCurrentPosition();
+    // Position? position = await Geolocator.getCurrentPosition(); // SOAL 12
+    Position position = await Geolocator.getCurrentPosition(); // SOAL 13
     return position;
   }
 }
