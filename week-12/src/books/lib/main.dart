7fce16fc181494b70699e9de0a8be69c26d64a81
SALMA ANNISSA AZIZI <salmaannisaazizi@gmail.com>
2023-11-25 16:19:25 +0700

    W12: Soal 11 dan 12

--- week-12/src/books/lib/main.dart
+++ week-12/src/books/lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'package:async/async.dart';
+import 'package:books/geolocation.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
 import 'package:http/http.dart' as http;
@@ -20,7 +21,8 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: const FuturePage(),
+      //home: const FuturePage(),
+      home : LocationScreen(),
     );
   }
 }
