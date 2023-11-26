995b125229fb666acc83857372234b7915b83edb
SALMA ANNISSA AZIZI <salmaannisaazizi@gmail.com>
2023-11-26 14:14:39 +0700

    W13: Soal 12

--- week-13/src/streambuilder_salma/lib/main.dart
+++ week-13/src/streambuilder_salma/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'stream.dart';
+import 'dart:async';
 
 void main() {
   runApp(const MyApp());
@@ -11,59 +13,52 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Stream | Salma Annissa',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.deepPurple,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const StreamHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
+class StreamHomePage extends StatefulWidget {
+  const StreamHomePage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<StreamHomePage> createState() => _StreamHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
+class _StreamHomePageState extends State<StreamHomePage> {
+  late Stream<int> numberStream;
   @override
+  void initState(){
+    numberStream = NumberStream().getNumbers();
+    super.initState();
+  }
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: const Text('Stream | Salma Annissa'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: StreamBuilder(
+        stream: numberStream,
+        initialData: 0,
+        builder: (context, snapshot){
+          if(snapshot.hasError) {
+            print('Error!');
+          }
+          if (snapshot.hasData){
+            return Center(
+              child: Text(
+                snapshot.data.toString(),
+                style: const TextStyle(fontSize: 96),
+              ));
+          } else {
+            return const SizedBox.shrink();
+          }
+        },
+      ),// This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
